dddecb9656a1b4a260291ce146b661a2b2cd3cc6
notarybob <serjoksalot@yahoo.com>
2025-03-23 00:44:07 +0000

    Change README - enhancing logs

--- src/utils/iPhoneX.ts
+++ src/utils/iPhoneX.ts
@@ -4,7 +4,7 @@ import { Dimensions, Platform } from 'react-native';
 // https://github.com/ptelad/react-native-iphone-x-helper
 
 export function isIphoneX() {
-  let dimen = Dimensions.get('window');
+  const dimen = Dimensions.get('window');
   return (
     Platform.OS === 'ios' &&
     !Platform.isPad &&
